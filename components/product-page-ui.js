9c7c27107769daf9b800aecff55e3454a63d138a
paltyshev <aleksep@gmail.com>
2023-06-18 16:03:56 +0300

    2 files modified, 1 added

--- components/product-page-ui.js
+++ components/product-page-ui.js
@@ -1,42 +1,41 @@
-import * as React from 'react'
-import { useRouter } from 'next/router'
-import Image from 'next/image'
-import { useCart } from 'react-use-cart'
-import dynamic from 'next/dynamic'
+import * as React from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { useCart } from 'react-use-cart';
+import dynamic from 'next/dynamic';
 
-import Button from '@/ui/button'
-import { ChevronDownSmallIcon } from '@/icons'
-import { formatCurrencyValue } from '@/utils/format-currency-value'
-import { useSettingsContext } from '@/context/settings'
-import ProductContent from './product-content'
-import ImageGallery from 'react-image-gallery'
-import "react-image-gallery/styles/css/image-gallery.css";
+import Button from '@/ui/button';
+import { ChevronDownSmallIcon } from '@/icons';
+import { formatCurrencyValue } from '@/utils/format-currency-value';
+import { useSettingsContext } from '@/context/settings';
+import ProductContent from './product-content';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import QuantitySelector from './quantity-selector';
 
 const ProductReviews = dynamic(() => import('@/components/product-reviews'), {
-  loading: () => <p>Загрузка...</p>
-})
+  loading: () => <p>Загрузка...</p>,
+});
 
 function ProductPageUI({ product }) {
-  const { addItem } = useCart()
-  const router = useRouter()
-  const { activeCurrency } = useSettingsContext()
-  const [variantQuantity, setVariantQuantity] = React.useState(1)
+  const { addItem } = useCart();
+  const router = useRouter();
+  const { activeCurrency } = useSettingsContext();
   const [activeVariantId, setActiveVariantId] = React.useState(
     router.query.variantId || (product.variants && product.variants[0]?.id)
-  )
+  );
+  const [selectedQuantity, setSelectedQuantity] = React.useState(1);
 
   React.useEffect(() => {
-    const url = `/products/${product.slug}?variant=${activeVariantId}`
+    const url = `/products/${product.slug}?variant=${activeVariantId}`;
 
-    router.replace(url, url, { shallow: true })
-  }, [activeVariantId])
+    router.replace(url, url, { shallow: true });
+  }, [activeVariantId]);
 
   const activeVariant = product.variants.find(
     (variant) => variant.id === activeVariantId
-  )
-  const updateQuantity = (event) =>
-    setVariantQuantity(Number(event.target.value))
-  const updateVariant = (event) => setActiveVariantId(event.target.value)
+  );
+  const updateVariant = (event) => setActiveVariantId(event.target.value);
 
   const addToCart = () => {
     const itemMetadata = router.locales.reduce(
@@ -45,11 +44,11 @@ function ProductPageUI({ product }) {
         [locale]: {
           ...product.localizations.find(
             (localization) => localization.locale === locale
-          )
-        }
+          ),
+        },
       }),
       {}
-    )
+    );
 
     addItem(
       {
@@ -57,13 +56,13 @@ function ProductPageUI({ product }) {
         productId: product.id,
         image: product.images[0],
         price: product.price,
-        ...itemMetadata
+        ...itemMetadata,
       },
-      variantQuantity
-    )
+      selectedQuantity
+    );
 
-    router.push('/cart')
-  }
+    router.push('/cart');
+  };
 
   const images = product.images.map((image) => ({
     original: image.url,
@@ -72,7 +71,11 @@ function ProductPageUI({ product }) {
     originalWidth: image.width,
     originalTitle: product.name,
     originalAlt: product.name,
-  }))
+  }));
+
+  const handleQuantityChange = (quantity) => {
+    setSelectedQuantity(quantity);
+  };
 
   return (
     <div>
@@ -99,7 +102,7 @@ function ProductPageUI({ product }) {
                   width={item.originalWidth}
                   height={item.originalHeight}
                   title={item.originalTitle}
-                  sizes='100vw'
+                  sizes="100vw"
                 />
               )}
               showThumbnails={false}
@@ -118,7 +121,7 @@ function ProductPageUI({ product }) {
             <p className="font-semibold text-xl leading-8 text-yellow-500">
               {formatCurrencyValue({
                 currency: activeCurrency,
-                value: product.price
+                value: product.price,
               })}
             </p>
           </div>
@@ -166,62 +169,41 @@ function ProductPageUI({ product }) {
               </label>
 
               <div className="flex space-x-2">
-                <div className="flex-none relative">
-                  <select
-                    id="quantity"
-                    name="quantity"
-                    value={variantQuantity}
-                    className="h-12 block appearance-none w-full bg-gainsboro border-2 border-gainsboro focus:border-slategray px-4 py-3 pr-8 focus:outline-none focus:bg-white text-slategray focus:text-slategray rounded-lg"
-                    onChange={updateQuantity}
-                  >
-                    {Array.from({ length: 5 }, (_, i) => {
-                      const value = Number(i + 1)
-
-                      return (
-                        <option key={value} value={value}>
-                          {value}
-                        </option>
-                      )
-                    })}
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
-                    <ChevronDownSmallIcon
-                      className="h-4 w-4 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </div>
-                </div>
+                <QuantitySelector
+                  onChange={handleQuantityChange}
+                  defaultValue={1}
+                />
                 <div className="flex-auto">
-                  <Button onClick={addToCart}>В корзину <svg
-                    className="ml-2"
-                    width="25"
-                    height="24"
-                    viewBox="0 0 25 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M12.5 2C10.2909 2 8.5 3.79086 8.5 6V7H7.5C5.84315 7 4.5 8.34315 4.5 10V19C4.5 20.6569 5.84315 22 7.5 22H17.5C19.1569 22 20.5 20.6569 20.5 19V10C20.5 8.34315 19.1569 7 17.5 7H16.5V6C16.5 3.79086 14.7091 2 12.5 2ZM14.5 9V11H16.5V9H17.5C18.0523 9 18.5 9.44772 18.5 10V19C18.5 19.5523 18.0523 20 17.5 20H7.5C6.94772 20 6.5 19.5523 6.5 19V10C6.5 9.44772 6.94772 9 7.5 9H8.5V11H10.5V9H14.5ZM14.5 7V6C14.5 4.89543 13.6046 4 12.5 4C11.3954 4 10.5 4.89543 10.5 6V7H14.5Z"
-                      fill="#212121"
-                    />
-                  </svg></Button>
+                  <Button onClick={addToCart}>
+                    В корзину{' '}
+                    <svg
+                      className="ml-2"
+                      width="25"
+                      height="24"
+                      viewBox="0 0 25 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12.5 2C10.2909 2 8.5 3.79086 8.5 6V7H7.5C5.84315 7 4.5 8.34315 4.5 10V19C4.5 20.6569 5.84315 22 7.5 22H17.5C19.1569 22 20.5 20.6569 20.5 19V10C20.5 8.34315 19.1569 7 17.5 7H16.5V6C16.5 3.79086 14.7091 2 12.5 2ZM14.5 9V11H16.5V9H17.5C18.0523 9 18.5 9.44772 18.5 10V19C18.5 19.5523 18.0523 20 17.5 20H7.5C6.94772 20 6.5 19.5523 6.5 19V10C6.5 9.44772 6.94772 9 7.5 9H8.5V11H10.5V9H14.5ZM14.5 7V6C14.5 4.89543 13.6046 4 12.5 4C11.3954 4 10.5 4.89543 10.5 6V7H14.5Z"
+                        fill="#212121"
+                      />
+                    </svg>
+                  </Button>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       </div>
-      {product.content && (
-        <ProductContent product={product} />
-      )}
+      {product.content && <ProductContent product={product} />}
       <div className="my-8">
         <ProductReviews product={product} />
       </div>
     </div>
-  )
+  );
 }
 
-export default ProductPageUI
+export default ProductPageUI;
---
+++ components/quantity-selector.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const QuantitySelector = ({ onChange, defaultValue = 1 }) => {
+  const [quantity, setQuantity] = useState(defaultValue);
+
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+    onChange(quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+      onChange(quantity - 1);
+    }
+  };
+
+  return (
+    <div className="flex items-center">
+      <button
+        className="h-12 bg-gainsboro border-2 border-gainsboro border-r-0 text-gray-700 font-bold py-2 px-4 rounded-l-lg"
+        onClick={handleDecrease}
+      >
+        -
+      </button>
+      <input
+        className="h-12 appearance-none  bg-gainsboro border-l-0 border-r-0 border-2 border-gainsboro py-2 px-4 rounded-none w-10 text-center"
+        value={quantity}
+        onChange={(e) => {
+          const value = parseInt(e.target.value);
+          setQuantity(value || 1);
+          onChange(value || 1);
+        }}
+      />
+      <button
+        className="h-12 border-2 border-gainsboro border-l-0 text-gray-700 font-bold py-2 px-4 rounded-r-lg"
+        onClick={handleIncrease}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default QuantitySelector;
