e72857d2751d684a2da20a042e0f8b5f9f4d38f4
paltyshev <aleksep@gmail.com>
2023-06-26 15:41:04 +0300

    5 files modified

--- components/header.js
+++ components/header.js
@@ -1,38 +1,37 @@
-import Link from 'next/link'
-import { useCart } from 'react-use-cart'
+import Link from 'next/link';
+import { useCart } from 'react-use-cart';
 
-import { formatCurrencyValue } from '@/utils/format-currency-value'
-import { HygraphSVG } from '@/svgs'
-import { ShoppingCartIcon } from '@/icons'
-import { useSettingsContext } from '@/context/settings'
+import { formatCurrencyValue } from '@/utils/format-currency-value';
+import { HygraphSVG } from '@/svgs';
+import { ShoppingCartIcon } from '@/icons';
+import { useSettingsContext } from '@/context/settings';
 
 function Header({ pages = [] }) {
-  const { cartTotal } = useCart()
-  const { activeCurrency } = useSettingsContext()
+  const { cartTotal } = useCart();
+  const { activeCurrency } = useSettingsContext();
 
   return (
     <header className="max-w-7xl mx-auto bg-white flex-grow flex items-center justify-between px-4 sm:px-6">
       <div className="py-6 w-full">
         <nav className="flex items-center justify-between flex-wrap space-x-4">
-          <Link href="/">
-            <HygraphSVG className="h-auto text-primary w-5" />
+          <Link
+            href="/"
+            className="flex items-center text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full px-3 font-medium"
+          >
+            <HygraphSVG className="h-auto text-primary w-5 flex-shrink-0" />{' '}
+            ToyBeary
           </Link>
           {pages.length ? (
             <ul className="hidden md:mx-auto md:block md:flex-grow">
-              <li
-                  key="1"
-                  className="block my-4 md:inline-block md:my-0"
-                >
-                  <Link href="/" className="text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full py-2 px-3 font-medium">
-                    ToyBeary
-                  </Link>
-                </li>
               {pages.map((page) => (
                 <li
                   key={page.id}
                   className="block my-4 md:inline-block md:my-0"
                 >
-                  <Link href={`/${page.type.toLowerCase()}/${page.slug}`} className="text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full py-2 px-3 font-medium">
+                  <Link
+                    href={`/${page.type.toLowerCase()}/${page.slug}`}
+                    className="text-lightgray hover:text-slategray hover:bg-gainsboro rounded-full py-2 px-3 font-medium"
+                  >
                     {page.name}
                   </Link>
                 </li>
@@ -48,7 +47,7 @@ function Header({ pages = [] }) {
               <span className="text-gray-900">
                 {formatCurrencyValue({
                   currency: activeCurrency,
-                  value: cartTotal
+                  value: cartTotal,
                 })}
               </span>
             </Link>
@@ -56,7 +55,7 @@ function Header({ pages = [] }) {
         </nav>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
